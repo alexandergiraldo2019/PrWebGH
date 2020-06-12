ed2911fdafb54cd566047489c967329c8a3e615c
Elkin Alexander Giraldo Ordoñez <alex_giraldo@hotmail.com>
2020-06-12 15:41:55 -0500

    Agregar archivos de proyecto.

---
+++ PrWebGH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrWebGH.Startup))]
+namespace PrWebGH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
