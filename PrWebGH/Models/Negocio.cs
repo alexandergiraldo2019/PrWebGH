34cd7dcbba37535f24c506ff0d81f7e6f17a4123
Elkin Alexander Giraldo Ordoñez <alex_giraldo@hotmail.com>
2020-06-12 15:46:25 -0500

    version 1.1

---
+++ PrWebGH/Models/Negocio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrWebGH.Models
+{
+    public class Negocio
+    {
+        public int  Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
